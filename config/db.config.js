55975634436e06811f15fa62d7f228bf0d511d72
rao <narenthira.rao@gmail.com>
2020-03-07 07:58:30 +0800

    online db

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "ufinity",
+    HOST: "us-cdbr-iron-east-04.cleardb.net",
+    USER: "b2eeafdfd6c9f5",
+    PASSWORD: "deeca5e8",
+    DB: "heroku_1682c7f0ee08936",
     dialect: "mysql",
     pool: {
       max: 5,
@@ -11,3 +11,5 @@ module.exports = {
       idle: 10000
     }
   };
+
+  // mysql://b2eeafdfd6c9f5:deeca5e8@us-cdbr-iron-east-04.cleardb.net/heroku_1682c7f0ee08936?reconnect=true
